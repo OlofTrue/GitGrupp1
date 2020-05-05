22dd35654d5bfa732bbf58432fea3d6a44791353
JoJo <unslavedcoder@gmail.com>
2020-05-05 15:14:23 +0200

    created employee class

---
+++ ConsoleApp2/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Employee
+    {
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+
+    }
+}
