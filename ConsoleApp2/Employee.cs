db27bf9426166db400c3ed25826dd5f1c815edec
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-05-05 15:31:50 +0200

    anderz2

--- ConsoleApp2/Employee.cs
+++ ConsoleApp2/Employee.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
+
+//anderz dhwqeodhqwihqwidhgq
+
 namespace ConsoleApp2
 {
     class Employee
     {
         public string Name { get; set; }
+
+        //anderz dhwqeodhqwihqwidhgq efdpweofiweouwheouhweouvhwefvuh
 
         public double Salary { get; set; }
 
